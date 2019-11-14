fd1088a5c30c4e9f24092cec742465ba9c56d190
Christopher Sandoval <san13660@uvg.edu.gt>
2019-11-13 23:13:16 -0600

    cambios

---
+++ lab_8/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Calculator from './components/body/body'
+
+function App() {
+  return (
+    <Calculator />
+  )
+}
+
+export default App
